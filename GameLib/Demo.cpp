978f5b0099e35cf67436b0e251459ec6e7bcca3d
eclipseflower <eclipseflowernju@gmail.com>
2017-02-12 21:33:28 +0800

    Add Color Demo

--- GameLib/Demo.cpp
+++ GameLib/Demo.cpp
@@ -8,6 +8,7 @@ ID3DXMesh *Objects[5] = { 0, 0, 0, 0, 0 };
 ID3DXMesh *Teapot = 0;
 
 D3DXMATRIX ObjWorldMatrices[5];
+D3DXMATRIX WorldMatrix;
 
 const int Width = 800;
 const int Height = 600;
@@ -19,6 +20,16 @@ struct Vertex {
 	}
 	float _x, _y, _z;
 	static const DWORD FVF = D3DFVF_XYZ;
+};
+
+struct ColorVertex {
+	ColorVertex() {}
+	ColorVertex(float x, float y, float z, D3DCOLOR c) {
+		_x = x; _y = y; _z = z; _color = c;
+	}
+	float _x, _y, _z;
+	D3DCOLOR _color;
+	static const DWORD FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
 };
 
 bool Cube_Setup() {
@@ -219,6 +230,42 @@ bool Triangle_Display(float timeDelta) {
 	return true;
 }
 
+bool Color_Setup() {
+	Device->CreateVertexBuffer(3 * sizeof(ColorVertex), D3DUSAGE_WRITEONLY, ColorVertex::FVF,
+		D3DPOOL_MANAGED, &Triangle, 0);
+	ColorVertex *v;
+	Triangle->Lock(0, 0, (void **)&v, 0);
+	v[0] = ColorVertex(-1.0f, 0.0f, 2.0f, D3DCOLOR_XRGB(255, 0, 0));
+	v[1] = ColorVertex(0.0f, 1.0f, 2.0f, D3DCOLOR_XRGB(0, 255, 0));
+	v[2] = ColorVertex(1.0f, 0.0f, 2.0f, D3DCOLOR_XRGB(0, 0, 255));
+	Triangle->Unlock();
+
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(&proj, D3DX_PI * 0.5f, (float)Width / (float)Height, 1.0f, 1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+	Device->SetRenderState(D3DRS_LIGHTING, false);
+	return true;
+}
+
+bool Color_Display(float timeDelta) {
+	Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+	Device->BeginScene();
+	Device->SetFVF(ColorVertex::FVF);
+	Device->SetStreamSource(0, Triangle, 0, sizeof(ColorVertex));
+	D3DXMatrixTranslation(&WorldMatrix, -1.25f, 0.0f, 0.0f);
+	Device->SetTransform(D3DTS_WORLD, &WorldMatrix);
+	Device->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_FLAT);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+	D3DXMatrixTranslation(&WorldMatrix, 1.25f, 0.0f, 0.0f);
+	Device->SetTransform(D3DTS_WORLD, &WorldMatrix);
+	Device->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_GOURAUD);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+	Device->EndScene();
+	Device->Present(0, 0, 0, 0);
+	return true;
+}
+
 //
 // WinMain
 //
@@ -235,8 +282,8 @@ int WINAPI WinMain(HINSTANCE hinstance,
 		return 0;
 	}
 
-	Triangle_Setup();
-	D3DLib::EnterMsgLoop(Triangle_Display);
+	Color_Setup();
+	D3DLib::EnterMsgLoop(Color_Display);
 
 	Device->Release();
 
