08bd089f42dce668a22e146dfa1f30b7c8fafebd
EclipseFlower <eclipseflowernju@gmail.com>
2017-03-23 21:45:59 +0800

    Chapter 5 demo complete

--- GameLib/D3DDemo.cpp
+++ GameLib/D3DDemo.cpp
@@ -497,11 +497,145 @@ bool TexQuad_Setup() {
 }
 
 bool TexQuad_Display(float timeDelta) {
+	if (GetAsyncKeyState('W') & 0x8000f) {
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);
+	}
+	if (GetAsyncKeyState('B') & 0x8000f) {
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_BORDER);
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_BORDER);
+		Device->SetSamplerState(0, D3DSAMP_BORDERCOLOR, 0x000000ff);
+	}
+	if (GetAsyncKeyState('C') & 0x8000f) {
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_CLAMP);
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_CLAMP);
+	}
+	if (GetAsyncKeyState('M') & 0x8000f) {
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_MIRROR);
+		Device->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_MIRROR);
+	}
+
 	Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
 	Device->BeginScene();
 	Device->SetStreamSource(0, Quad, 0, sizeof(TexVertex));
 	Device->SetFVF(TexVertex::FVF);
 	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
+	Device->EndScene();
+	Device->Present(0, 0, 0, 0);
+	return true;
+}
+
+bool TexCube_Setup() {
+	Device->CreateVertexBuffer(24 * sizeof(TexVertex), D3DUSAGE_WRITEONLY, Vertex::FVF, 
+		D3DPOOL_MANAGED, &VB, 0);
+	TexVertex *v;
+	VB->Lock(0, 0, (void **)&v, 0);
+	v[0] = TexVertex(-1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[1] = TexVertex(-1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[2] = TexVertex(1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+	v[3] = TexVertex(1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[4] = TexVertex(-1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+	v[5] = TexVertex(1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+	v[6] = TexVertex(1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+	v[7] = TexVertex(-1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+
+	v[8] = TexVertex(-1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f);
+	v[9] = TexVertex(-1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f);
+	v[10] = TexVertex(1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f);
+	v[11] = TexVertex(1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+
+	v[12] = TexVertex(-1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f);
+	v[13] = TexVertex(1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f);
+	v[14] = TexVertex(1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f);
+	v[15] = TexVertex(-1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f);
+
+	v[16] = TexVertex(-1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[17] = TexVertex(-1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[18] = TexVertex(-1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[19] = TexVertex(-1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	v[20] = TexVertex(1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[21] = TexVertex(1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[22] = TexVertex(1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[23] = TexVertex(1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	VB->Unlock();
+
+	Device->CreateIndexBuffer(36 * sizeof(WORD), D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED,
+		&IB, 0);
+	WORD *i;
+	IB->Lock(0, 0, (void **)&i, 0);
+	i[0] = 0; i[1] = 1; i[2] = 2;
+	i[3] = 0; i[4] = 2; i[5] = 3;
+	i[6] = 4; i[7] = 5; i[8] = 6;
+	i[9] = 4; i[10] = 6; i[11] = 7;
+	i[12] = 8; i[13] = 9; i[14] = 10;
+	i[15] = 8; i[16] = 10; i[17] = 11;
+	i[18] = 12; i[19] = 13; i[20] = 14;
+	i[21] = 12; i[22] = 14; i[23] = 15;
+	i[24] = 16; i[25] = 17; i[26] = 18;
+	i[27] = 16; i[28] = 18; i[29] = 19;
+	i[30] = 20; i[31] = 21; i[32] = 22;
+	i[33] = 20; i[34] = 22; i[35] = 23;
+	IB->Unlock();
+
+	D3DLIGHT9 light;
+	ZeroMemory(&light, sizeof(light));
+	light.Type = D3DLIGHT_DIRECTIONAL;
+	light.Ambient = D3DXCOLOR(0.8f, 0.8f, 0.8f, 1.0f);
+	light.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	light.Specular = D3DXCOLOR(0.2f, 0.2f, 0.2f, 1.0f);
+	light.Direction = D3DXVECTOR3(1.0f, -1.0f, 0.0f);
+	Device->SetLight(0, &light);
+	Device->LightEnable(0, true);
+
+	Device->SetRenderState(D3DRS_NORMALIZENORMALS, true);
+	Device->SetRenderState(D3DRS_SPECULARENABLE, true);
+
+	D3DXCreateTextureFromFile(Device, L"crate.jpg", &Tex);
+	Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR);
+
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(&proj, D3DX_PI * 0.5f, (float)Width / (float)Height, 1.0f, 1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+	return true;
+}
+
+bool TexCube_Display(float timeDelta) {
+	static float angle = (3.0f * D3DX_PI) / 2.0f;
+	static float height = 2.0f;
+
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000f)
+		angle -= 0.5f * timeDelta;
+
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000f)
+		angle += 0.5f * timeDelta;
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000f)
+		height += 5.0f * timeDelta;
+
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000f)
+		height -= 5.0f * timeDelta;
+
+	D3DXVECTOR3 position(cosf(angle) * 3.0f, height, sinf(angle) * 3.0f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &position, &target, &up);
+	Device->SetTransform(D3DTS_VIEW, &V);
+
+	Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+	Device->BeginScene();
+	D3DMATERIAL9 mtrl = D3DLib::InitMaterial(D3DLib::WHITE, D3DLib::WHITE, D3DLib::WHITE, 
+		D3DLib::BLACK, 2.0f);
+	Device->SetMaterial(&mtrl);
+	Device->SetTexture(0, Tex);
+	Device->SetStreamSource(0, VB, 0, sizeof(TexVertex));
+	Device->SetIndices(IB);
+	Device->SetFVF(TexVertex::FVF);
+	Device->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 24, 0, 12);
 	Device->EndScene();
 	Device->Present(0, 0, 0, 0);
 	return true;
@@ -520,8 +654,8 @@ int D3DDemo(HINSTANCE hinstance,
 		return 0;
 	}
 
-	TexQuad_Setup();
-	D3DLib::EnterMsgLoop(TexQuad_Display);
+	TexCube_Setup();
+	D3DLib::EnterMsgLoop(TexCube_Display);
 
 	Device->Release();
 
