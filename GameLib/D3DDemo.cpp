f40b3e35697c4d97db2944bbc47dd66f4de65909
EclipseFlower <eclipseflowernju@gmail.com>
2017-03-28 20:37:19 +0800

    chapter 7 demo complete

--- GameLib/D3DDemo.cpp
+++ GameLib/D3DDemo.cpp
@@ -7,8 +7,11 @@ IDirect3DVertexBuffer9 *Triangle = 0;
 IDirect3DVertexBuffer9 *Pyramid = 0;
 IDirect3DVertexBuffer9 *Quad = 0;
 IDirect3DVertexBuffer9 *BkGndQuad = 0;
+IDirect3DVertexBuffer9 *BackDropVB = 0;
 IDirect3DTexture9 *BkGndTex = 0;
 IDirect3DTexture9 *Tex = 0;
+IDirect3DTexture9 *CrateTex = 0;
+IDirect3DTexture9 *BackDropTex = 0;
 ID3DXMesh *Objects[5] = { 0, 0, 0, 0, 0 };
 ID3DXMesh *Teapot = 0;
 D3DMATERIAL9 TeapotMtrl;
@@ -17,6 +20,7 @@ D3DMATERIAL9 Mtrl[5];
 
 D3DXMATRIX ObjWorldMatrices[5];
 D3DXMATRIX WorldMatrix;
+D3DXMATRIX CubeWorldMatrix;
 
 const int Width = 800;
 const int Height = 600;
@@ -54,10 +58,13 @@ struct TexVertex {
 	TexVertex() {}
 	TexVertex(float x, float y, float z, float nx, float ny, float nz, float u, float v) {
 		_x = x; _y = y; _z = z; _nx = nx; _ny = ny; _nz = nz; _u = u; _v = v;
+		//_color = D3DCOLOR_RGBA(255,255,255,100);
 	}
 	float _x, _y, _z;
 	float _nx, _ny, _nz;
+	//D3DCOLOR _color;
 	float _u, _v;
+	// remember: must D3DFVF_DIFFUSE then D3DFVF_TEX1
 	static const DWORD FVF = D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_TEX1;
 };
 
@@ -530,7 +537,7 @@ bool TexQuad_Display(float timeDelta) {
 }
 
 bool TexCube_Setup() {
-	Device->CreateVertexBuffer(24 * sizeof(TexVertex), D3DUSAGE_WRITEONLY, Vertex::FVF, 
+	Device->CreateVertexBuffer(24 * sizeof(TexVertex), D3DUSAGE_WRITEONLY, TexVertex::FVF,
 		D3DPOOL_MANAGED, &VB, 0);
 	TexVertex *v;
 	VB->Lock(0, 0, (void **)&v, 0);
@@ -722,12 +729,150 @@ bool MtrlAlpha_Display(float timeDelta) {
 	Device->SetTexture(0, BkGndTex);
 	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
 
+	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
 	D3DXMatrixScaling(&W, 1.5f, 1.5f, 1.5f);
 	Device->SetTransform(D3DTS_WORLD, &W);
 	Device->SetMaterial(&TeapotMtrl);
 	Device->SetTexture(0, 0);
 	Teapot->DrawSubset(0);
+	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
+
+	Device->EndScene();
+	Device->Present(0, 0, 0, 0);
+	return true;
+}
+
+bool TexAlpha_Setup() {
+	Device->CreateVertexBuffer(6 * sizeof(TexVertex), D3DUSAGE_WRITEONLY, TexVertex::FVF,
+		D3DPOOL_MANAGED, &BackDropVB, 0);
+
+	TexVertex* v;
+	BackDropVB->Lock(0, 0, (void**)&v, 0);
+
+	// quad built from two triangles, note texture coordinates:
+	v[0] = TexVertex(-10.0f, -10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[1] = TexVertex(-10.0f, 10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[2] = TexVertex(10.0f, 10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[3] = TexVertex(-10.0f, -10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[4] = TexVertex(10.0f, 10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[5] = TexVertex(10.0f, -10.0f, 5.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+
+	BackDropVB->Unlock();
+
+	Device->CreateVertexBuffer(24 * sizeof(TexVertex), D3DUSAGE_WRITEONLY, TexVertex::FVF,
+		D3DPOOL_MANAGED, &VB, 0);
+	VB->Lock(0, 0, (void **)&v, 0);
+	v[0] = TexVertex(-1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[1] = TexVertex(-1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[2] = TexVertex(1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+	v[3] = TexVertex(1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[4] = TexVertex(-1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+	v[5] = TexVertex(1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+	v[6] = TexVertex(1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+	v[7] = TexVertex(-1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+
+	v[8] = TexVertex(-1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f);
+	v[9] = TexVertex(-1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f);
+	v[10] = TexVertex(1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f);
+	v[11] = TexVertex(1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+
+	v[12] = TexVertex(-1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f);
+	v[13] = TexVertex(1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f);
+	v[14] = TexVertex(1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f);
+	v[15] = TexVertex(-1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f);
+
+	v[16] = TexVertex(-1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[17] = TexVertex(-1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[18] = TexVertex(-1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[19] = TexVertex(-1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	v[20] = TexVertex(1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[21] = TexVertex(1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[22] = TexVertex(1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[23] = TexVertex(1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	VB->Unlock();
+
+	Device->CreateIndexBuffer(36 * sizeof(WORD), D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED,
+		&IB, 0);
+	WORD *i;
+	IB->Lock(0, 0, (void **)&i, 0);
+	i[0] = 0; i[1] = 1; i[2] = 2;
+	i[3] = 0; i[4] = 2; i[5] = 3;
+	i[6] = 4; i[7] = 5; i[8] = 6;
+	i[9] = 4; i[10] = 6; i[11] = 7;
+	i[12] = 8; i[13] = 9; i[14] = 10;
+	i[15] = 8; i[16] = 10; i[17] = 11;
+	i[18] = 12; i[19] = 13; i[20] = 14;
+	i[21] = 12; i[22] = 14; i[23] = 15;
+	i[24] = 16; i[25] = 17; i[26] = 18;
+	i[27] = 16; i[28] = 18; i[29] = 19;
+	i[30] = 20; i[31] = 21; i[32] = 22;
+	i[33] = 20; i[34] = 22; i[35] = 23;
+	IB->Unlock();
+
+	D3DXCreateTextureFromFile(Device, L"cratewalpha.dds", &CrateTex);
+	D3DXCreateTextureFromFile(Device, L"lobbyxpos.jpg", &BackDropTex);
+
+	Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_POINT);
+
+	Device->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+	Device->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_SELECTARG1);
+
+	Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+	Device->SetRenderState(D3DRS_LIGHTING, false);
+
+	D3DXVECTOR3 pos(0.0f, 0.f, -2.5f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &pos, &target, &up);
+	Device->SetTransform(D3DTS_VIEW, &V);
+
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(&proj, D3DX_PI * 0.5f, (float)Width / (float)Height, 1.0f, 1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+	return true;
+}
+
+bool TexAlpha_Display(float timeDelta) {
+	D3DXMATRIX xRot;
+	D3DXMatrixRotationX(&xRot, D3DX_PI * 0.2f);
+
+	static float y = 0.0f;
+	D3DXMATRIX yRot;
+	D3DXMatrixRotationY(&yRot, y);
+	y += timeDelta;
+
+	if (y >= 6.28f)
+		y = 0.0f;
+
+	CubeWorldMatrix = xRot * yRot;
+
+	Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xff0000ff, 1.0f, 0);
+	Device->BeginScene();
+	D3DXMATRIX I;
+	D3DXMatrixIdentity(&I);
+	Device->SetStreamSource(0, BackDropVB, 0, sizeof(TexVertex));
+	Device->SetFVF(TexVertex::FVF);
+	Device->SetTexture(0, BackDropTex);
+	Device->SetTransform(D3DTS_WORLD, &I);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
+
 	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	Device->SetTransform(D3DTS_WORLD, &CubeWorldMatrix);
+	//Device->SetMaterial(0);
+	Device->SetTexture(0, CrateTex);
+	Device->SetStreamSource(0, VB, 0, sizeof(TexVertex));
+	Device->SetIndices(IB);
+	Device->SetFVF(TexVertex::FVF);
+	Device->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 24, 0, 12);
+	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
 
 	Device->EndScene();
 	Device->Present(0, 0, 0, 0);
@@ -747,8 +892,8 @@ int D3DDemo(HINSTANCE hinstance,
 		return 0;
 	}
 
-	MtrlAlpha_Setup();
-	D3DLib::EnterMsgLoop(MtrlAlpha_Display);
+	TexAlpha_Setup();
+	D3DLib::EnterMsgLoop(TexAlpha_Display);
 
 	Device->Release();
 
